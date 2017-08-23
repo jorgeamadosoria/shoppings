828546de1d30c474ca1326762fc4571d91774976
jorge.amado <darksoul.uci@gmail.com>
2017-08-23 15:15:48 -0300

    formatting changes

--- routes/brands.js
+++ routes/brands.js
@@ -4,34 +4,34 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/list', function(req, res, next) {
-  listService.listBrand(function(brands){
-    res.render("brands/list",{"brands":brands});
-  });
+    listService.listBrand(function(brands) {
+        res.render("brands/list", { "brands": brands });
+    });
 });
 
 router.get('/detail/:id', function(req, res, next) {
-  res.render("brands/detail",{ "num":100, "units":listService.findBrandById(req.params.id)});
+    res.render("brands/detail", { "num": 100, "units": listService.findBrandById(req.params.id) });
 });
 
 router.delete('/:id', function(req, res, next) {
-  
+
 });
 
 router.get('/form', function(req, res, next) {
-  if (req.query.id)
-    res.render("brands/form",listService.findBrandById(req.query.id));
-  else
-    res.render("brands/form");
+    if (req.query.id)
+        res.render("brands/form", listService.findBrandById(req.query.id));
+    else
+        res.render("brands/form");
 });
 
 router.post('/form', function(req, res, next) {
 
-  if (req.body.id)
-    listService.updateBrand(req.body.id,req.body);
-  else
-    listService.insertBrand(req.body);
+    if (req.body.id)
+        listService.updateBrand(req.body.id, req.body);
+    else
+        listService.insertBrand(req.body);
 
-  res.redirect("/brands/list");
+    res.redirect("/brands/list");
 });
 
 
--- services/lists.js
+++ services/lists.js
@@ -1,66 +1,66 @@
 var units = require("../data/units.mock");
 var brandModel = require('../data/brand');
 
-var handleError = function (err) {
-  console.log("ERROR:" + err);
-  return null;
+var handleError = function(err) {
+    console.log("ERROR:" + err);
+    return null;
 };
 
-var insertCallback = function (err, object) {
-  if (err) return handleError(err);
-  console.log("saved:" + object);
+var insertCallback = function(err, object) {
+    if (err) return handleError(err);
+    console.log("saved:" + object);
 };
 
-var updateCallback = function (err, object) {
-  if (err) return handleError(err);
-  console.log("updated:" + object);
+var updateCallback = function(err, object) {
+    if (err) return handleError(err);
+    console.log("updated:" + object);
 };
 
-var removeCallback = function (err, object) {
-  if (err) return handleError(err);
-  console.log("removed:" + object);
+var removeCallback = function(err, object) {
+    if (err) return handleError(err);
+    console.log("removed:" + object);
 };
 
-var findCallback = function (err, docs) {
-  if (err)
-    return handleError(err);
-  console.log("result:" + JSON.stringify(docs));
-  return true;
+var findCallback = function(err, docs) {
+    if (err)
+        return handleError(err);
+    console.log("result:" + JSON.stringify(docs));
+    return true;
 };
 
 module.exports = {
 
-  getUnits: function () {
+    getUnits: function() {
 
-    return units;
-  },
+        return units;
+    },
 
-  insertBrand: function (brand) {
-    brandModel.create(brand, insertCallback);
-  },
+    insertBrand: function(brand) {
+        brandModel.create(brand, insertCallback);
+    },
 
-  updateBrand: function (brandId, brand) {
-    brandModel.findByIdAndUpdate(brandId, brand, updateCallback);
-  },
+    updateBrand: function(brandId, brand) {
+        brandModel.findByIdAndUpdate(brandId, brand, updateCallback);
+    },
 
-  deleteBrand: function (brandId) {
-    brandModel.findByIdAndRemove(brandId, removeCallback);
-  },
+    deleteBrand: function(brandId) {
+        brandModel.findByIdAndRemove(brandId, removeCallback);
+    },
 
-  listBrand: function (renderCallback) {
+    listBrand: function(renderCallback) {
 
-    brandModel.find().lean().exec(function (err, docs) { if (findCallback(err, docs)) renderCallback(docs); });
+        brandModel.find().lean().exec(function(err, docs) { if (findCallback(err, docs)) renderCallback(docs); });
 
-  },
+    },
 
-  findBrandById: function (brandId) {
-    var result = null;
-    brandModel.findById(brandId, function (err, doc) {
-      if (err)
-        return handleError(err);
-      console.log("found:" + doc);
-      result = doc;
-    });
-    return result;
-  }
+    findBrandById: function(brandId) {
+        var result = null;
+        brandModel.findById(brandId, function(err, doc) {
+            if (err)
+                return handleError(err);
+            console.log("found:" + doc);
+            result = doc;
+        });
+        return result;
+    }
 };
