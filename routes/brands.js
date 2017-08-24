613949b6613803d0ad826caf0133bfed508de05e
jorge.amado <darksoul.uci@gmail.com>
2017-08-24 14:15:36 -0300

    added font awesome, bootstrap, jquery and popper

--- routes/brands.js
+++ routes/brands.js
@@ -2,46 +2,51 @@ var express = require('express');
 var listService = require('../services/lists');
 var router = express.Router();
 
-router.get('/list', function (req, res, next) {
-    listService.listBrand().then(function (brands) {
+var handleError = function(err) {
+    console.log("ERROR:" + err);
+    return null;
+};
+
+router.get('/list', function(req, res, next) {
+    listService.listBrand().then(function(brands) {
         res.render("brands/list", {
             "brands": brands
         });
-    });
+    }, handleError());
 });
 
-router.get('/detail/:id', function (req, res, next) {
-    listService.findBrandById(req.params.id).then(function (doc) {
-        res.render("details/", doc);
-    });
+router.get('/detail/:id', function(req, res, next) {
+    listService.findBrandById(req.params.id).then(function(brand) {
+        res.render("brands/detail", brand);
+    }, handleError());
 });
 
-router.delete('/:id', function (req, res, next) {
-    listService.deleteBrand(req.params.id).then(function(doc){
+router.delete('/:id', function(req, res, next) {
+    listService.deleteBrand(req.params.id).then(function(brand) {
         res.sendStatus(200).end();
-    });
-    
+    }, handleError());
+
 });
 
-router.get('/form', function (req, res, next) {
+router.get('/form', function(req, res, next) {
     if (req.query.id)
-        listService.findBrandById(req.query.id).then(function (doc) {
-            res.render("brands/form", doc);
-        });
+        listService.findBrandById(req.query.id).then(function(brand) {
+            res.render("brands/form", brand);
+        }, handleError());
     else
         res.render("brands/form");
 });
 
-router.post('/form', function (req, res, next) {
+router.post('/form', function(req, res, next) {
 
     if (req.query.id)
-        listService.updateBrand(req.query.id, req.body).then(function (doc) {
+        listService.updateBrand(req.query.id, req.body).then(function(brand) {
             res.redirect("list");
-        });
+        }, handleError());
     else
-        listService.insertBrand(req.body).then(function (doc) {
+        listService.insertBrand(req.body).then(function(brand) {
             res.redirect("list");
-        });
+        }, handleError());
 });
 
 module.exports = router;
--- services/lists.js
+++ services/lists.js
@@ -1,64 +1,37 @@
 var units = require("../data/units.mock");
 var brandModel = require('../data/brand');
 
-var handleError = function (err) {
-    console.log("ERROR:" + err);
-    return null;
-};
-
-var insertCallback = function (err, object) {
-    if (err) return handleError(err);
-    console.log("saved:" + object);
-};
-
-var updateCallback = function (err, object) {
-    if (err) return handleError(err);
-    console.log("updated:" + object);
-};
-
-var removeCallback = function (err, object) {
-    if (err) return handleError(err);
-    console.log("removed:" + object);
-};
-
-var findCallback = function (err, docs) {
-    if (err)
-        return handleError(err);
-    console.log("result:" + JSON.stringify(docs));
-    return true;
-};
-
 module.exports = {
 
-    getUnits: function () {
+    getUnits: function() {
 
         return units;
     },
 
-    insertBrand: function (brand) {
+    insertBrand: function(brand) {
         brandModel.create(brand, insertCallback);
     },
 
-    updateBrand: function (brandId, brand) {
+    updateBrand: function(brandId, brand) {
         var id = mongoose.Types.ObjectId(brandId);
         brand._id = id;
         return brandModel.findByIdAndUpdate(id, brand).exec();
     },
 
-    deleteBrand: function (brandId) {
+    deleteBrand: function(brandId) {
         var id = mongoose.Types.ObjectId(brandId);
         return brandModel.findByIdAndRemove(id).exec();
     },
 
-    listBrand: function () {
+    listBrand: function() {
 
         return brandModel.find().lean().exec();
 
     },
 
-    findBrandById: function (brandId) {
+    findBrandById: function(brandId) {
         var id = mongoose.Types.ObjectId(brandId);
-        return brandModel.findById(id).lean().exec(function (err, doc) {
+        return brandModel.findById(id).lean().exec(function(err, doc) {
             if (err)
                 return handleError(err);
             console.log("found:" + doc);
