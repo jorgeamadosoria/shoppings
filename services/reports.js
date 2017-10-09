fd956e455df5468ffe8d95d63f68f8990f9cc79f
jorgeamadosoria <darksoul.uci@gmail.com>
2017-10-09 19:01:17 -0300

    WIP monthlyTotal

--- services/reports.js
+++ services/reports.js
@@ -1,4 +1,5 @@
 var model = require('../data/item');
+var moment = require('moment');
 
 module.exports = {
     categoriesChart: function(currency) {
@@ -6,5 +7,12 @@ module.exports = {
     },
     reasonsChart: function(currency) {
         return model.aggregate([{ $match: { currency: currency } }, { $group: { _id: { value: "$reason", currency: "$currency" }, total: { $sum: "$item_cost" } } }]).exec();
-    }
+    },
+
+    monthlyTotal: function(currency,month) {
+        var fromD =  moment(month + "-01");
+        var from = fromD.format("YYYY-MM-DD");
+        var to = fromD.add(1,"month").format("YYYY-MM-DD");
+        return model.aggregate([{ $match: {$and: [{date:{"$gte" : from}},{date:{"$lt" : to}}]} }, { $group: { _id: { currency: "$currency" }, total: { $sum: "$item_cost" } } }]).exec();
+    },
 };
