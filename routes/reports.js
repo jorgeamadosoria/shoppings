97384d07bb845b8d72a5b16ae418c64cdb2577b5
jorge.amado <darksoul.uci@gmail.com>
2017-10-11 18:41:01 -0300

    finished data massaging and  rough layering on reports. Finer CSS is still required, as well as null validation and rounding for currency values.

--- routes/reports.js
+++ routes/reports.js
@@ -35,10 +35,39 @@ router.get('/mreport', function(req, res, next) {
     var itemsPromise = service.itemList(req.query.currency, req.query.month).then(obj => data.items = obj, handleError);
     Promise.all([categoriesPromise, reasonsPromise, addressPromise, itemsPromise, dailyTotalPromise, monthlyTotalPromise]).then(function(obj) {
         data.dailyTotal = _.sortBy(data.dailyTotal, e => moment(e._id.value).utc());
+
+        var days = moment(data.dailyTotal[1]._id.value).daysInMonth();
         data.dailyTotal = _.map(data.dailyTotal, function(e) {
             return { _id: { value: moment(e._id.value).utc().date() }, total: e.total };
         });
-        data.topItems = _.sortBy(data.items, e => e.totalItemCost * -1)
+
+        var dailyTotalTemp = new Array(days + 1);
+        _.each(data.dailyTotal, e => dailyTotalTemp[e._id.value] = e);
+        _.each(dailyTotalTemp, e => console.log(e));
+        data.dailyTotal = _.map(dailyTotalTemp, function(e, i) {
+            if (!e) return { _id: { value: i }, total: 0 };
+            else return e;
+        }).slice(1, days + 1);
+
+        data.categoriesChart10 = [];
+        if (data.categoriesChart10.length < 10) {
+            for (i = 0; i < 10; i++)
+                if (i < data.categoriesChart.length)
+                    data.categoriesChart10.push(data.categoriesChart[i]);
+                else
+                    data.categoriesChart10.push({ _id: { value: "-" }, total: "-" });
+        }
+
+        data.reasonsChart10 = [];
+        if (data.reasonsChart10.length < 10) {
+            for (i = 0; i < 10; i++)
+                if (i < data.reasonsChart.length)
+                    data.reasonsChart10.push(data.reasonsChart[i]);
+                else
+                    data.reasonsChart10.push({ _id: { value: "-" }, total: "-" });
+        }
+
+        data.topItems = _.sortBy(data.items, e => e.totalItemCost * -1);
         data.topItems = _.filter(data.topItems, (e, i) => i < 10);
         data.dayMax = _.max(data.dailyTotal, e => e.total).total;
         data.monthlyMedian = lists.median(_.map(data.dailyTotal, e => moment(e.total).utc()));
