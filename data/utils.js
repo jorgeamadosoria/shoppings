465e96b83c0eed66aa6b6ee372f82fdae7879109
jorgeamadosoria <darksoul.uci@gmail.com>
2017-10-31 00:05:43 -0300

    added flaw for easier debugging by bypassing user autuh

--- data/utils.js
+++ data/utils.js
@@ -7,18 +7,24 @@ module.exports = {
         service.delete(req.params.id).then(function(obj) {
             res.sendStatus(200).end();
         }, handleError);
-
     },
 
-
     loggedRole: function(roles) {
+        //debugging switch
+        var alwaysLoggedIn = true;
+
         return function(req, res, next) {
-            res.locals.user = req.user;
-            if (req.user && req.isAuthenticated() && _.contains(roles, req.user.role))
+            if (!alwaysLoggedIn){
+
+                res.locals.user = req.user;
+                if (req.user && req.isAuthenticated() && _.contains(roles, req.user.role))
+                    return next();
+                res.redirect('/login');
+            }
+            else{
+                res.locals.user = {google:{name:"DEBUG",email:"email"},role:"admin"};
                 return next();
-            res.redirect('/login');
-            //      res.locals.user = {google:{name:"DEBUG",email:"email"},role:"admin"};
-            //      return next();
+            }
         }
     },
 
